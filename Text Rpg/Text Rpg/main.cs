74935fb2f17e6b6e519b21f1e20c896fac0f3dde
Kattemadsguden <144560726+Kattemadsguden@users.noreply.github.com>
2025-09-26 13:48:51 +0200

    Ryddede op i lidt kode

--- Text Rpg/Text Rpg/main.cs
+++ Text Rpg/Text Rpg/main.cs
@@ -1,21 +1,31 @@
 ﻿
+// Dialouge start
 
-DialougeSystem NewDialouge = new DialougeSystem("Narrator", "You wake up cold and alone");
+DialougeSystem NarratorDialouge1 = new DialougeSystem("Narrator", "You wake up cold and alone in a dark and moist basement. You can sense an evil energy in the room.");
+DialougeSystem NarratorDialouge2 = new DialougeSystem("Narrator", "You look around and spot a door, and a window.");
+
+// Dialouge end 
+
+
 HpSystem Health = new HpSystem();
+
+NarratorDialouge1.displayDialouge();
+NarratorDialouge2.displayDialouge();
 
 PlayerChoice();
 void PlayerChoice()
 {
-    Console.WriteLine("Tast 1 eller 2");
+    Console.WriteLine("Press 1 to jump out of the window | Press 2 to go through the door");
     int spillervalg = int.Parse(Console.ReadLine());
-    
+
     if (spillervalg == 1)
     {
         Health.HP += -5;
-        Console.WriteLine("you lost 5 hp");
-        Health.HpReader();
-    }
+        Console.WriteLine("you lost 5 hp | You now have " + Health.HP + " HP");
 
+    }
+    else if (spillervalg == 2) ;
 }
+
 
 
